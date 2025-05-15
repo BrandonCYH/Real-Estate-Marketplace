8b48449c0bf45acc8ef5f811814bebee62338c4e
BRANDON CYH <chengyangho14@gmaile.com>
2025-05-15 17:46:01 +0800

    Setting up project

---
+++ src/main/java/com/realestatemarketplace/realestatemarketplace/RealEstateMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.realestatemarketplace.realestatemarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateMarketplaceApplication.class, args);
+    }
+
+}
