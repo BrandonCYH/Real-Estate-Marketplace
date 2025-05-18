e8b8552824bba2d90c3f966c08533ff6509f3cd7
BRANDON CYH <chengyangho14@gmaile.com>
2025-05-18 10:52:27 +0800

    Update React File

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from 'react-router-dom';
+import './App.css';
+import './index.css';
+import HomePage from "./Pages/HomePage";
+import AboutUs from "./Pages/AboutUs";
+import LoginPage from "./Pages/LoginPage";
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/real-estate-marketplace" element={<HomePage/>}/>
+                <Route path="/about-us" element={<AboutUs/>}/>
+                <Route path="/sign-in" element={<LoginPage/>}/>
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
